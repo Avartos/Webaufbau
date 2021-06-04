4f49cb81abb782846f40dfca469088eb211d9cd6
Avartos <59014772+Avartos@users.noreply.github.com>
2021-06-04 11:09:38 +0200

    lodIn und sigin components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,10 +2,9 @@ import React, { useState } from "react";
 
 const Login = () => {
 
-    const UserLogin = ({handleLoginForm}) => {
-        const [userName, setUserName] = useState("");
-        const [password, setPassword] = useState("");
-    }
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    
 
     return ( 
         <div className = "Login">
@@ -13,14 +12,14 @@ const Login = () => {
                 <span className = "LoginTitle">Login</span>
              </div>
              <form onSubmit = {(e) =>{
-                 handleLoginForm(e, userName, password);
+                 //handleLoginForm(e, userName, password);
                  setUserName("");
                  setPassword("");
              }}>
 
              <div>
                  <label className = "LoginLabel">Benutzername: </label>
-                 <input className = "LoginInput"
+                 <input placeholder="Benutzername" className = "LoginInput"
                  type = "text"
                  value = {userName}
                  onChange = {(e) => {
@@ -30,12 +29,14 @@ const Login = () => {
              
              <div>
                  <label className = "LoginLabel">Password: </label>
-                 <input className = "LoginInput"
-                 type = "text"
-                 value = {password}
-                 onChange = {(e) => {
-                     setPassword(e.target.value);
-                 }}></input>
+                 <input  placeholder="Passwort" className = "LoginInput"
+                    type = "password"
+                    value = {password}
+                    onChange = {(e) => {
+                        setPassword(e.target.value);
+                    }}>
+
+                    </input>    
              </div>
 
              <div>
@@ -46,6 +47,8 @@ const Login = () => {
              <div>
                  <a href = "/">Registrierung</a>
              </div>
+             <p>{userName}</p>
+             <p>{password}</p>
         </div>
      );
 }
--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,7 +1,55 @@
 const Signin = () => {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordTwo, setPasswordTwo] = useState("");
+
     return ( 
         <div className = "Sigin">
+            <div className = "LoginHeader">
+                            <span className = "LoginTitle">Registrierung</span>
+                        </div>
+                        <form onSubmit = {(e) =>{
+                            
+                        }}>
 
+                        <div>
+                            <label className = "LoginLabel">Benutzername: </label>
+                            <input placeholder="Benutzername" className = "LoginInput"
+                            type = "text"
+                            value = {userName}
+                            onChange = {(e) => {
+                                setUserName(e.target.value);
+                            }}></input>
+                        </div>
+                        
+                        <div>
+                            <label className = "LoginLabel">Password: </label>
+                            <input placeholder="Passwort" className = "LoginInput"
+                                type = "password"
+                                value = {password}
+                                onChange = {(e) => {
+                                    setPassword(e.target.value);
+                                }}>
+
+                                </input>    
+                        </div>
+
+                        <div>
+                            <label className = "LoginLabel">Password: </label>
+                            <input placeholder="nochmal das Passwort"className = "LoginInput"
+                                type = "password"
+                                value = {password}
+                                onChange = {(e) => {
+                                    setPasswordTwo(e.target.value);
+                                }}>
+
+                                </input>    
+                        </div>
+
+                        <div>
+                            <button className = "LoginButton">Registriere mich!</button>
+                        </div>
+                    </form>
                 
 
         </div>
