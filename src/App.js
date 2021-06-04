8ad54b5554154513b79b143e6823259179dd68d5
Robin <robin.beck.we@gmail.com>
2021-06-04 15:11:14 +0200

    #4 changed something

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import ThreadList from './components/threadList';
 
 function App() {
   return (
-  );
+    <div className="App">
+      
+      </div>
+    );
 }
 
 export default App;
