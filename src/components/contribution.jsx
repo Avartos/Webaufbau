2cec9125b0f258fcc9e2da0cce46af2b83ac9634
Saskia Wohlers <saskia.wohlers@fh-erfurt.de>
2021-06-23 23:25:04 +0200

    Funktionalität hinzugefügt

--- src/components/contribution.jsx
+++ src/components/contribution.jsx
@@ -2,26 +2,63 @@ import React, { useState } from 'react'
 import ReplyIcon from '@material-ui/icons/Reply';
 import RemoveIcon from '@material-ui/icons/Remove';
 import AddIcon from '@material-ui/icons/Add';
+import { Create } from '@material-ui/icons';
+import CreatedContribution from './createContribution';
+import Contributions from './contributions';
 
 
 
-function Contribution() {
-
-    function ContributorSquid(props) {
-        return <p>{props.contributorSquid}</p>
-    }
-
-    function ContributionText(props) {
-        return <p>{props.conText}</p>
-    }
+function Contribution({ contribution, isReply = false }) {
 
     const [count, setCount] = useState(0);
+    const [reply, setReply] = useState(false);
+    const [replies, setReplies] = useState(contribution.replies || []);
+
+    const AddNewContributionForm = () => {
+        const [contributionText, setContributionText] = useState("");
+        const [currentUser, setCurrentUser] = useState("Squidy50");
+        return (
+            <div className="newContributionForm">
+
+                <form className="body"
+                    onSubmit={(e) => {
+                        handleSubmitForm(e, contributionText, currentUser);
+                        setContributionText("");
+                    }}
+                >
+
+                    <textarea
+                        required
+                        value={contributionText}
+                        onChange={(e) => {
+                            setContributionText(e.target.value);
+                        }}
+                        placeholder="Gib deinen Beitrag zum Thema!"
+                    ></textarea>
+
+                    <button>Absenden</button>
+                </form>
+            </div>
+        )
+    }
+
+    const handleSubmitForm = (e, contributionText, currentUser) => {
+        e.preventDefault();
+        let newContribution = {
+            //id: nextId,
+            contributionText: contributionText,
+            contributorSquid: currentUser,
+        }
+
+        setReplies([...replies, newContribution]);
+    }
 
     return (
         <div className="contribution">
-            <p className="contributorSquid">From: <ContributorSquid contributorSquid="Squid161" /></p>
-            <p className="conText"><ContributionText conText="Hallo ich bin ein Beitrag!" /></p>
-            <button className="contributionButtons"> <ReplyIcon /> </button>
+            <p className="contributorSquid">From: {contribution.contributorSquid}</p>
+            <p className="conText">{contribution.contributionText}</p>
+            {!isReply && !reply && <button className="contributionButtons" onClick={() => setReply(true)}> <ReplyIcon /> </button>}
+            {!isReply && reply && <div><AddNewContributionForm /><button onClick={() => setReply(false)}>Verwerfen</button></div>}
 
             <div id="CounterOfLikes">
 
@@ -30,7 +67,18 @@ function Contribution() {
                 <button className="contributionButtons" onClick={() => setCount(count + 1)}> <AddIcon /> </button>
 
             </div>
+            <div className="replies">
+                {replies.map((reply) => {
+                    return (
+                        <Contribution
+                            contribution={reply}
+                            isReply={true}
+                        />
+                    );
+                })}
+            </div>
         </div>
     )
 }
+
 export default Contribution;
