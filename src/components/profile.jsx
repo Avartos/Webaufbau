c1c606f891da93ad192c9a8a3bb82fe0d7680875
F0rkster <matthias-gabel@hotmail.de>
2021-07-01 00:46:04 +0200

    scss profile

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -2,6 +2,7 @@ import { ReactComponent as ProfilePicture} from "../assets/icons/profilePicture.
 import React, {useState} from "react";
 import ProfileStats from "./profileStats"
 import ProfileButtons from "./profileButtons"
+import "../assets/css/_profile.scss"
 
 
 
@@ -14,19 +15,37 @@ const Profile = (props) => {
     {
         return(
             <div className="wrapper-account">
-                <ProfileStats username={username} email={email} />
-                <ProfilePicture/>
-                <ProfileButtons roll={props.roll}/>
+                <div className="header">
+                    <h2>Userverwaltung</h2>
+                </div>
+                <div className="body">
+                    <div className="left">
+                        <ProfileStats username={username} email={email} />
+                        <ProfileButtons roll={props.roll}/>
+                    </div>
+                    <div className="right">
+                        <ProfilePicture/>
+                    </div>
+                </div>
             </div>
         );
     } else if (props.roll == "admin")
     {
         return(
             <div className="wrapper-account">
-                <input type="text"/>
-                <ProfileStats username={username} email={email} />
-                <ProfilePicture/>
-                <ProfileButtons roll={props.roll}/>
+                <div className="header">
+                    <h2>Userverwaltung</h2>
+                </div>
+                <div className="body">
+                    <div className="left">
+                        <input type="text"/>
+                        <ProfileStats username={username} email={email} />
+                        <ProfileButtons roll={props.roll}/>
+                    </div>
+                    <div className="right">
+                        <ProfilePicture/>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/profileButtons.jsx
+++ src/components/profileButtons.jsx
@@ -1,3 +1,4 @@
+import "../assets/css/_profileButtons.scss"
 
 
 
