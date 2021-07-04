a6019935e405b4e6ab6a8b08a17b9aba71b3211d
Robin <robin.beck.we@gmail.com>
2021-07-04 10:01:54 +0200

    removed checkbox from favbar

--- src/components/favList.jsx
+++ src/components/favList.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { CSSTransition } from "react-transition-group";
+
 import { ReactComponent as SplatIcon } from "../assets/icons/splat.svg";
+import FavThreadList from "./favThreadList";
 
 const FavList = (props) => {
-  //state = {};
+  
 
   return (
     <div className="favList">
@@ -11,32 +14,9 @@ const FavList = (props) => {
           <SplatIcon className="splatIcon" />
           <span>{props.className}</span>
         </div>
-        {/*forEach list*/}
         {props.list.map((item) => {
           return (
-            <React.Fragment>
-              <input
-                type="checkbox"
-                id={`forumCheckBox${item.id}`}
-                className="checkbox"
-              />
-              <li className="favForum" key={item.id}>
-                <label htmlFor={`forumCheckBox${item.id}`}>
-                  <span>{item.forum}</span>
-                </label>
-                <ul>
-                  {/*forEach threads*/}
-                  {item.threads.map((thread) => {
-                    return (
-                      <li className="favThread" key={thread}>
-                        {" "}
-                        {thread}{" "}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </li>
-            </React.Fragment>
+            <FavThreadList item={item} key={item.key}></FavThreadList>
           );
         })}
       </ul>
